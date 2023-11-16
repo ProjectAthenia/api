4416a41369bd0c466302887eb772823ec3105b27
Bryce Meyer <bryce13950@gmail.com>
2023-11-16 22:17:18 +0100

    added owned by validator

---
+++ code/app/Validators/OwnedByValidator.php
@@ -0,0 +1,52 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Validators;
+
+use Illuminate\Http\Request;
+use Illuminate\Validation\Validator;
+
+/**
+ * Class OwnedByValidator
+ * @package App\Validators
+ */
+class OwnedByValidator
+{
+    /**
+     * @var Request
+     */
+    protected $request;
+
+    /**
+     * OwnedByValidator constructor.
+     * @param Request $request
+     */
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    /**
+     * This is invoked by the validator rule 'owned_by'
+     *
+     * @param $attribute
+     * @param $value
+     * @param array $parameters
+     * @param Validator|null $validator
+     * @return bool
+     */
+    public function validate($attribute, $value, $parameters = [], Validator $validator = null)
+    {
+        $ownerRequestParamName = array_shift($parameters);
+
+        $ownerParam = $this->request->route($ownerRequestParamName);
+
+        while (count($parameters) ) {
+            $relation = array_shift($parameters);
+            $relatedObject = $ownerParam->{$relation};
+            $ownerParam = $relatedObject;
+        }
+
+        return $relatedObject->contains('id', $value);
+    }
+}
---
+++ code/tests/Unit/Validators/OwnedByValidatorTest.php
@@ -0,0 +1,59 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\Unit\Validators;
+
+use App\Models\Asset;
+use App\Models\Organization\Organization;
+use App\Models\Payment\Payment;
+use App\Models\Payment\PaymentMethod;
+use App\Validators\OwnedByValidator;
+use Illuminate\Http\Request;
+use Tests\TestCase;
+
+/**
+ * Class OwnedByTest
+ * @package Tests\Unit\Validators
+ */
+class OwnedByValidatorTest extends TestCase
+{
+    public function testOwnedByValidatorFalse()
+    {
+        $paymentMethod = new PaymentMethod();
+        $paymentMethod->id = 2345;
+        $organization = new Organization([
+            'paymentMethods' => collect(),
+        ]);
+        $organization->id = 234;
+
+        $request = mock(Request::class);
+        $request->shouldReceive('route')->with('organization')->andReturn($organization);
+
+        $ownedBy = new OwnedByValidator($request);
+
+        $params = ['organization','paymentMethods'];
+
+        $this->assertFalse($ownedBy->validate('payment_method.1', 2345, $params));
+    }
+
+    public function testOwnedByValidatorTrue()
+    {
+        $paymentMethod = new PaymentMethod();
+        $paymentMethod->id = 2345;
+        $organization = new Organization([
+            'paymentMethods' => collect([
+                $paymentMethod,
+            ]),
+        ]);
+        $organization->id = 234;
+
+        $request = mock(Request::class);
+        $request->shouldReceive('route')->with('organization')->andReturn($organization);
+
+        $ownedBy = new OwnedByValidator($request);
+
+        $params = ['organization','paymentMethods'];
+
+        $this->assertTrue($ownedBy->validate('payment_method.1', 2345, $params));
+    }
+}
