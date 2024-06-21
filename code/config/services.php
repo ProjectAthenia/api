a2b5733c7d3be5b59fcd419796954e88d3776f22
Bryce Meyer <bryce13950@gmail.com>
2024-06-22 01:49:06 +0200

    added slack config

--- code/config/services.php
+++ code/config/services.php
@@ -33,6 +33,11 @@ return [
         'secret' => env('STRIPE_SECRET', 'sk_test'),
     ],
 
+    'slack' => [
+        'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
+        'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL', 'server-monitor'),
+    ],
+
     'fcm' => [
         'key' => env('FCM_KEY', ''),
     ],
