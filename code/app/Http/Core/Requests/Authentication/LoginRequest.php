af99cfd9f84be6a15a88d0b4d211e10371b9faf6
Bryce Meyer <bryce13950@gmail.com>
2025-03-07 19:06:32 +0100

    added auth routes

---
+++ code/app/Http/Core/Requests/Authentication/LoginRequest.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Http\Core\Requests\Authentication;
+
+use App\Athenia\Http\Core\Requests\Authentication\LoginRequest as BaseLoginRequest;
+
+/**
+ * Class LoginRequest
+ * @package App\Http\Core\Requests\Authentication
+ */
+class LoginRequest extends BaseLoginRequest
+{}
