68a5d9de52f8527d62614d78658335073b370806
Bryce Meyer <bryce13950@gmail.com>
2025-03-11 14:46:38 +0100

    migrated more requests

---
+++ code/app/Http/Core/Requests/Feature/ViewRequest.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Http\Core\Requests\Feature;
+
+use App\Athenia\Http\Core\Requests\Feature\ViewRequest as BaseViewRequest;
+
+/**
+ * Class ViewRequest
+ * @package App\Http\Core\Requests\Feature
+ */
+class ViewRequest extends BaseViewRequest
+{}
---
+++ code/app/Http/Core/Requests/ForgotPassword/ResetPasswordRequest.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Http\Core\Requests\ForgotPassword;
+
+use App\Athenia\Http\Core\Requests\ForgotPassword\ResetPasswordRequest as BaseResetPasswordRequest;
+
+/**
+ * Class ResetPasswordRequest
+ * @package App\Http\Core\Requests\ForgotPassword
+ */
+class ResetPasswordRequest extends BaseResetPasswordRequest
+{}
