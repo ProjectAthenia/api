13cb3a63eb98d49fef8bf4b4f147154c747bd0a7
Bryce Meyer <bryce13950@gmail.com>
2025-05-02 20:56:05 +0200

    removed extra constructor

--- code/app/Http/V1/Controllers/StatisticController.php
+++ code/app/Http/V1/Controllers/StatisticController.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 namespace App\Http\V1\Controllers;
 
 use App\Athenia\Http\Core\Controllers\StatisticControllerAbstract;
-use App\Athenia\Contracts\Repositories\Statistics\StatisticRepositoryContract;
 
 /**
  * Class StatisticController
@@ -12,12 +11,4 @@ use App\Athenia\Contracts\Repositories\Statistics\StatisticRepositoryContract;
  */
 class StatisticController extends StatisticControllerAbstract
 {
-    /**
-     * StatisticController constructor.
-     * @param StatisticRepositoryContract $repository
-     */
-    public function __construct(StatisticRepositoryContract $repository)
-    {
-        $this->repository = $repository;
-    }
 } 
